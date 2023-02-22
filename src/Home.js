06d975dd647e4e7785d095df0b33f4a4fc3e362b
Kegan <kegan.cardozo@ntlworld.com>
2023-02-22 23:07:00 +0000

    Commit from desktop

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
+import BlogList from './BlogList';
+import { useState } from "react";
 const Home = () => {
+
+const [blogs, setBlogs] = useState([
+    {title: "Guess how to mix coffee",body: "Yolo first blog",author: "mario", id:1},
+    {title: "You MUST READ THIS POST",body: "Yolo second blog",author: "Kegan", id:2},
+    {title: "OMG 10 ways to die",body:"Yolo third blog",author: "Adam", id:3}
+])
+
     return ( 
-        <div className="home">
+        <div className="content">
             <h2>Homepage</h2>
+           <BlogList blogs={blogs} title="All Blogs" />
+
         </div>
      );
 }
